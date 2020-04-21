098393733b54f9a71f2065e122462b29cd8b2a34
新城安基 <mittel@shinshiroyasumotonoMacBook-Pro.local>
2020-04-21 20:01:03 +0900

    InitalCommit

---
+++ app.js
@@ -0,0 +1,16 @@
+$(function(){
+	$('.box1').slideDown();
+});
+
+$(function(){
+	$('.box2').slideUp();
+});
+
+$(function(){
+	$('.box3').show();
+	$('.box3').css({'background-color': '#0000FF'});
+});
+
+$(function(){
+	$(.box4).hide();
+});
